7f6c75b1515d7029ae267b2f9a9b33e78795ff51
Gabriel Schippers <schippers_gabriel@mail.com>
2017-08-18 04:20:01 +0200

    add function of React bubble increasing in the center

--- src/App.js
+++ src/App.js
@@ -1,41 +1,180 @@
 import React, { Component } from 'react';
+import update from 'react-addons-update';
 import logo from './logo.svg';
 import './App.css';
-import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
-import {pulse} from 'react-animations'
-import Radium from 'radium';
 
-const styles = {
- pulse: {
-   animation: 'x 1s infinite',
-   animationName: Radium.keyframes(pulse, 'pulse')
- }
-}
-console.log("Pulse", styles);
-
+const maxVelocity = 1
 
 const circleData = {
   "price": [15, 30, 45, 60, 75],
   "name": ["Shorts", "Shoe", "Cap", "Tennis", "Pants"]
 }
 class App extends Component {
+
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      circleStyleArr: [],
+      keyPos: 0,
+      timeOfLastUpdate: -1
+    }
+
+    this.tick = this.tick.bind(this)
+  }
+  /*
   state = {
     circleStyleArr: [],
     keyPos: 0,
+    timeOfLastUpdate: -1
+  }
+*/
+  componentDidMount () {
+    this.movementTimer = setInterval(this.tick, 33);
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.movementTimer)
+  }
+
+
+  checkBoundaryCollision(newLeft, newTop, circle) {
+    let resolvedParameters = {
+      left: newLeft,
+      top: newTop,
+      movementVector: circle.movementVector
+    }
+
+    //check left/right boundaries
+    if ((newLeft <= 0) || (newLeft + circle.width > 1920)) {
+      resolvedParameters.movementVector.x = resolvedParameters.movementVector.x * -1
+    }
+
+    //check top/bottom boundaries
+    if ((newTop < -30) || (newTop + circle.height > 805)) {
+      resolvedParameters.movementVector.y = resolvedParameters.movementVector.y * -1 
+    }
+
+    return resolvedParameters
+  }
+
+  // TODO: Resolve individual collisions
+  // This will consider the X anf Y components of each vector (movementVector * velocity) individually
+  handleParticleCollision(focusCircle, subjectCircle, focusMovementValues, focusPosition) {
+    switch(focusPosition) {
+      case 'top left':
+        if (focusMovementValues.movementVector.y >= 0) {
+
+        } else {
+
+        }
+        break;
+      case 'top right':
+
+        break;
+      case 'bottom left':
+
+        break;
+      case 'bottom right':
+
+        break;
+      default:
+        return focusMovementValues
+    }
+  }
+
+  checkParticleCollision(movementValues, circles) {
+    let adjustedMovementValues = movementValues
+
+    function checkVerticalParticalAlignment (focusCircle, subjectCircle, focusMovementValues, focusPosition) {
+      let newFocusMovementValues = focusMovementValues
+
+      if (focusMovementValues.top < subjectCircle.top) {
+        if ((focusMovementValues.top + focusCircle.height) > subjectCircle.top) {
+          let collisionPosition = 'top ' + focusPosition
+          newFocusMovementValues = this.handleParticleCollision(focusCircle, subjectCircle, focusMovementValues, collisionPosition)      
+        }
+      } else {
+        if ((subjectCircle.top + subjectCircle.height) > focusMovementValues.top) {
+          let collisionPosition = 'bottom' + focusPosition
+          newFocusMovementValues = this.handleParticleCollision(focusCircle, subjectCircle, focusMovementValues, collisionPosition) 
+        }
+      }
+
+      return newFocusMovementValues
+    }
+    
+    for (let i = 0; i < circles.length; i++) {
+      for (let j = 0; j < circles.length; j++) {
+        if (i != j) {
+          //check collision
+          let focusCircle = circles[i]
+          let subjectCircle = circles[j]
+
+          if (movementValues.left < subjectCircle.left) {
+            if ((movementValues.left + focusCircle.width) > subjectCircle.left) {
+              //check vertical
+              adjustedMovementValues = checkVerticalParticalAlignment(focusCircle, subjectCircle, movementValues, 'left')
+            }
+          } else {
+            if ((subjectCircle.left + subjectCircle.width) > movementValues.left) {
+              //check vertical
+              adjustedMovementValues = checkVerticalParticalAlignment(focusCircle, subjectCircle, movementValues, 'right')
+            }
+          }
+        }
+        return adjustedMovementValues
+      }
+    }
+  }
+
+  tick () {
+    let newCircleStyleArr = this.state.circleStyleArr
+
+    for (let i = 0; i < newCircleStyleArr.length; i++) {
+      let circle = newCircleStyleArr[i] || {}
+
+      let newLeft = circle.left + (circle.velocity * circle.movementVector.x)
+      let newTop = circle.top + (circle.velocity * circle.movementVector.y)
+
+      let adjustedMovementValues = this.checkBoundaryCollision(newLeft, newTop, circle)
+      //adjustedMovementValues = this.checkParticleCollision(adjustedMovementValues, this.state.circleStyleArr)
+
+      circle.left = adjustedMovementValues.left
+      circle.top = adjustedMovementValues.top
+      circle.movementVector = adjustedMovementValues.movementVector
+    }
+
+    if (this.state.timeOfLastUpdate === -1) {
+      this.setState({
+        circleStyleArr: newCircleStyleArr,
+        timeOfLastUpdate: new Date() - this.state.timeOfLastUpdate
+      })
+    } else {
+      this.setState({
+        timeOfLastUpdate: new Date()
+      })
+    }
   }
 
   addCircle = () => {
     var tempArr = [];
-
+    let isStart = false;
+    //check start or add bubble
     if (this.state.keyPos > 0) {
-      tempArr = this.state.circleStyleArr;
+      tempArr = [...this.state.circleStyleArr];
 
       tempArr.shift();    
       tempArr.push(this.genStyle(tempArr));
     }
     else {     
+      isStart = true;
       for ( var i = 0; i < parseInt(this.refs['count'].value); i++ ) {
-        tempArr.push(this.genStyle(tempArr))
+        tempArr.push({
+          ...this.genStyle(tempArr),
+          left: this.getRandomX(),
+          top: this.getRandomY(),
+        });
       }
     }
     
@@ -43,43 +182,69 @@ class App extends Component {
       circleStyleArr: tempArr,
       keyPos: this.state.keyPos+1,
     });
+
+    setTimeout(() => {
+      if (!isStart) {
+        this.setState(update(this.state, { circleStyleArr: { [tempArr.length - 1]: { $merge: {
+          left: this.getRandomX(),
+          top: this.getRandomY(),
+          transition: 'left 1s, top 1s',
+        }} } }));
+      }
+    }, 4000);
   }
 
   getRandomColor = () => {
     return '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
   }
 
+  getRandomX = () => Math.floor(Math.random() * (window.innerWidth-250))
+
+  getRandomY = () => Math.floor(Math.random() * (window.innerHeight-460))
+
+  getRandomVectorComponent() {
+    let magnitude = Math.random()
+    let sign = Math.random() > 0.5 ? 1 : -1
+
+    magnitude = magnitude * sign
+
+    return magnitude
+  }
+
   genStyle = (circleArr) => {
-    var left, top, circleSize, circleColor;
+    var left, top, circleSize, circleColor, fontSize, movementX, movementY, velocity;
 
     do {
-      left = Math.floor(Math.random() * (window.innerWidth-250));
-      top = Math.floor(Math.random() * (window.innerHeight-460));
-      circleSize = Math.floor(Math.random()*180+70);
+      left = this.getRandomX();
+      top = this.getRandomY();
+      circleSize = Math.floor(Math.random()*160+90);
       circleColor = this.getRandomColor();
-      // console.log("55555", left);
+      fontSize = circleSize * 0.6;
+      movementX = this.getRandomVectorComponent()
+      movementY = 1 - movementX
+      velocity = Math.random() * maxVelocity
     }
     while (this.isNotAvailable(circleArr, left, top, circleSize))
 
     return {
-      'left': left,
-      'top': top,
+      'left': window.innerWidth / 2 - circleSize / 2,
+      'top': 0,
       'width': circleSize,
       'height': circleSize,
       'backgroundColor': circleColor,
+      'fontSize': fontSize,
+      'moving': false,
+      'velocity': velocity,
+      'movementVector': {x:movementX,y:movementY}
     };
   }
 
   isNotAvailable = (posArr, newLeft, newTop, newSize) => {
-    console.log("6666", posArr);
     for (var i = 0; i < posArr.length; i ++) {
       const regLeft = posArr[i].left,
             regTop = posArr[i].top,
             regSize = posArr[i].width;
-      // console.log("22222", regLeft);
-      // console.log("new left", newLeft);
       if ((regLeft - newSize < newLeft && newLeft < regLeft + regSize) && (regTop-newSize < newTop && newTop < regTop+regSize)) {
-        // console.log("33333");
         return true;
       }
     }
@@ -88,7 +253,6 @@ class App extends Component {
   }
 
   render() {
-    console.log("111",this.state.circleStyleArr);
     
     return (
       <div className="App">
@@ -99,35 +263,28 @@ class App extends Component {
         </div>
 
         <div className="circle-Control">
-          <select defaultValue={4} onChange={this.selectChange} ref='count' className="Circle-count">
+          <select defaultValue={10} onChange={this.selectChange} ref='count' className="Circle-count">
             <option value={1}>1</option>
-            <option value={2}>2</option>
-            <option value={3}>3</option>
-            <option value={4}>4</option>
+            <option value={5}>5</option>
+            <option value={10}>10</option>
+            <option value={20}>20</option>
           </select>  
           <input onClick={this.addCircle} type="button" value={this.state.keyPos>0?"Add":"Start"} />
         </div>
 
         { 
           this.state.circleStyleArr.map((item, idx) => {
-            {/* var idx = Math.floor(Math.random() * 5); */}
             return (
-              <CSSTransitionGroup 
-                  key={`circle-item-${idx+this.state.keyPos}`}
-                  transitionName="anim" 
-                  transitionAppear={true} 
-                  transitionAppearTimeout={3000}
-                  transitionEnter={true}
-                  transitionEnterTimeout={3000} 
-                  transitionLeave={false} >
+              <div
+                  className={(this.state.keyPos > 1 && idx > this.state.circleStyleArr.length - this.state.keyPos) ? 'Add-Splash' : 'Circle-Splash'}
+                  key={`circle-item-${idx+this.state.keyPos}`}>
                 <div
-                    className='Circle'
-                    style={item} >
-                  <span className='Circle-Price'>{circleData.price[(idx+this.state.keyPos)%5]}</span> 
-                  <br/> 
-                  <span className='Circle-Detail'>{circleData.name[(idx+this.state.keyPos)%5]}</span>
+                  className='Circle'
+                  style={item} >
+                  <div className='Circle-Price'>{circleData.price[(idx+this.state.keyPos)%5]}</div> 
+                  <div className='Circle-Detail'>{circleData.name[(idx+this.state.keyPos)%5]}</div>
                 </div>
-              </CSSTransitionGroup>   
+              </div>
             )  
           })
         }
